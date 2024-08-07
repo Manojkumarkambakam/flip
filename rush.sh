2d226e111b67e263e53ce46054548f55666e3a91
Manojkumarkambakam <manojkambakam1338@gmail.com>
2024-08-07 15:04:09 +0530

    Update rush.sh

--- rush.sh
+++ rush.sh
@@ -3,3 +3,4 @@ this lin is also merging into main branch
 #including the files from master branch 
 #And also practicing the commands in git from linux.
 #And also practicing the coflicts session;and dividing into to barnches
+Addind the new line
