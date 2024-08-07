aede5f4da2873558de92cfc635fe2a98f8cd56f0
Manojkumarkambakam <Manojkumarkambakam>
2024-08-07 08:08:08 +0000

    Added

--- rush.sh
+++ rush.sh
@@ -1,3 +1,4 @@
+This line i sto check 
 #including the files from master branch 
 #And also practicing the commands in git from linux.
 #And also practicing the coflicts session;and dividing into to barnches
